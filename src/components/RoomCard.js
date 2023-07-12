edba81eee971206d85ac9a0b861f125a4a0425a2
Roy Kanghwan Kim <roykim0416@gmail.com>
2023-07-12 22:45:47 +1000

    card rearranged

--- src/components/RoomCard.js
+++ src/components/RoomCard.js
@@ -21,7 +21,7 @@ function RoomCard() {
         >
           <div className="roomCard">
             <div className="roomCard__img">
-              <img src={item.picture} alt="room-picture" />
+              <img src={item.picture} alt="room" />
             </div>
             <div className="roomCardSummaryContainer">
               <div
